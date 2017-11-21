7c0893b718d39b6875286cb7d905852b749fc523
monsieur.ahn <monsieur.ahn@gmail.com>
2017-11-21 20:23:26 +0900

    Info class - v0.1

--- Info.java
+++ Info.java
@@ -2,12 +2,24 @@ import java.util.ArrayList;
 
 import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
 import java.io.*;
 
-public class Info<T> {
+public class Info {
 	private File file;
-	private ArrayList<T> values;
+
+	FileInputStream inputStream;
+	FileOutputStream outputStream;
+	JsonReader reader;
+	JsonWriter writer;
+	JsonParser parser;
+	
+	JsonElement tree;
+	JsonArray array;
+	JsonObject object;
+	JsonElement element;
+	
 	
 	private Gson gson;
 	
@@ -16,69 +28,174 @@ public class Info<T> {
 	}
 	
 	public Info(String fileName) {
-		file = new File(fileName);
+		try {
+			file = new File(fileName);
+			file.createNewFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	public ArrayList<T> readJsonWith(Class<T> cls, String phoneNumber, double[] userLocation) {
+	public ArrayList<Cafe> readCafeListWith(double[] userLocation) {
+		ArrayList<Cafe> cafeList = new ArrayList<Cafe>();
+		Cafe cafeObj;
+		double dist;
+
 		try {
-			FileInputStream stream = new FileInputStream(file);
-			JsonReader reader = new JsonReader(new InputStreamReader(stream, "UTF-8"));
-			
-			values = new ArrayList<T>();
+			inputStream = new FileInputStream(file);
+			reader = new JsonReader(new InputStreamReader(inputStream, "UTF-8"));
 			
 			gson = new GsonBuilder().create();
-
+			
 			reader.beginArray();
 			
 			while(reader.hasNext()) {
-				T data = gson.fromJson(reader, cls);
+				cafeObj = gson.fromJson(reader, Cafe.class);
+				dist = Util.getDistance(cafeObj.getLocation(), userLocation);
 				
-				if(cls.getName() == "Cafe" && userLocation != null) {
-					double[] cafeLocation = ((Cafe) data).getLocation();
-					double dist = Util.getDistance(cafeLocation, userLocation);
-					
-					// if calculate distance is within 2000 meters
-					if (2000 > dist) {
-						values.add(data);
-					}
-					
-				} else if(cls.getName() == "User" && phoneNumber != null) {
-					if(phoneNumber.equals(((User) data).getInfo().get("phoneNumber"))) {
-						values.add(data);
-						break;
-					}
-				} else {
-					System.out.println("One of arguments necessary is missing");
+				if(2000 > dist) {
+					cafeList.add(cafeObj);
 				}
 			}
 			
 			reader.endArray();
 			reader.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return cafeList;
+	}
+	
+	public void writeCafetWith(Cafe cafeObj) {
+		try {
+			boolean found=false;
+			JsonElement cafeElement = null;
 			
-		} catch (Exception e) {
+			inputStream = new FileInputStream(file);
+			reader = new JsonReader(new InputStreamReader(inputStream, "UTF-8"));
+			parser = new JsonParser();
+			
+			tree = parser.parse(reader);
+			
+			inputStream.close();
+
+			outputStream = new FileOutputStream(file, false);
+			writer = new JsonWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+			
+			gson = new Gson();
+			cafeElement = gson.toJsonTree(cafeObj);
+
+			writer.setIndent("   ");
+			
+			if(tree.isJsonNull()) {
+				array = new JsonArray();
+			} else {
+				array = tree.getAsJsonArray();
+				
+				for(int i=0; i<array.size(); i++) {
+					element = array.get(i);
+					
+					if(element.isJsonObject()) {
+						object = element.getAsJsonObject();
+						
+						if(object.get("index").getAsInt() == cafeObj.getIndex()) {
+							found = true;
+							array.set(i, cafeElement);
+							break;
+						}
+					}
+				}
+			}
+
+			if(!found) {
+				array.add(cafeElement);
+			}
+
+			gson.toJson(array, writer);
+			
+			writer.close();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public User readUserInfoWith(String phoneNumber) {
+		User userObj=null, tempObj=null;
+		
+		try {
+			inputStream = new FileInputStream(file);
+			reader = new JsonReader(new InputStreamReader(inputStream, "UTF-8"));
+			
+			gson = new GsonBuilder().create();
+			
+			reader.beginArray();
+			
+			while(reader.hasNext()) {
+				tempObj = gson.fromJson(reader, User.class);
+				if(tempObj.getInfo().get("phoneNumber").equals(phoneNumber)) {
+					userObj = tempObj;
+					break;
+				}
+			}
+			
+			reader.endArray();
+			reader.close();
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		
-		return values;
-		
+		return userObj;
 	}
 	
-	public void writeJsonWith(ArrayList<T> values) {
+	public void writeUserInfoWith(User userObj) {
 		try {
+			boolean found=false;
+			JsonElement userElement = null;
 			
-			// read json file to find out overlapped value
-			/*
-			FileInputStream stream = new FileInputStream(file);
-			JsonReader reader = new JsonReader(new InputStreamReader(stream, "UTF-8"));
-			JsonParser parser = new JsonParser();
-			parser.parse(reader);
-			*/
-
-			FileWriter writer = new FileWriter(file);
-			gson = new GsonBuilder().setPrettyPrinting().create();
+			inputStream = new FileInputStream(file);
+			reader = new JsonReader(new InputStreamReader(inputStream, "UTF-8"));
+			parser = new JsonParser();
 			
-			gson.toJson(values, writer);
+			tree = parser.parse(reader);
+			
+			inputStream.close();
 
+			outputStream = new FileOutputStream(file, false);
+			writer = new JsonWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+			
+			gson = new Gson();
+			userElement = gson.toJsonTree(userObj);
+			
+			writer.setIndent("   ");
+			
+			if(tree.isJsonNull()) {
+				array = new JsonArray();
+			} else {
+				array = tree.getAsJsonArray();
+				
+				for(int i=0; i<array.size(); i++) {
+					element = array.get(i);
+					
+					if(element.isJsonObject()) {
+						object = element.getAsJsonObject();
+						
+						if(object.get("phoneNumber").getAsString().equals(userObj.getInfo().get("phoneNumber"))) {
+							found = true;
+							array.set(i, userElement);
+							break;
+						}
+					}
+				}
+			}
+
+			if(!found) {
+				array.add(userElement);
+			}
+
+			gson.toJson(array, writer);
+			
 			writer.close();
 			
 		} catch(Exception e) {
